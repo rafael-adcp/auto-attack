651061eaa8dd18b828eb1aa118a7d896b3505595
Rafael <rafael_adcp@hotmail.com>
2024-08-01 22:41:03 -0300

    making manager_cacarecos a bit more friendly

--- cacarecos.py
+++ cacarecos.py
@@ -5,7 +5,8 @@ import time
 def manager_cacarecos(event):
     while not event.is_set():
         if event.is_set():
-            return        
+            return
+        did_something = False
         if constants.VOCACAO_EM_USO == constants.Vocation.PALADIN:
             # se o quiver estiver vazio, refila ele
             if pyautogui.locateOnScreen('imgs/quiver_vazio.png', confidence=0.98, region=constants.REGION_QUIVER):
@@ -14,15 +15,18 @@ def manager_cacarecos(event):
                 pyautogui.press('num7')
                 pyautogui.press('num7')
                 pyautogui.press('num7')
+                did_something = True
 
         #mover esses cacarecos pra uma outra thread, isso aqui n deveria atrapalhar a rotacao de skill
         #apenas come qndo o icone de fome aparecer, evitar ficar spamando
         if pyautogui.locateOnScreen('imgs/starving.png', confidence=0.8):
             pyautogui.press('0') # mushroom
+            did_something = True
 
         # apenas usa utura gran, caso o icone nao esteja na barrinha de status
         if not pyautogui.locateOnScreen('imgs/utura_gran.png', confidence=0.98) and not constants.VOCACAO_EM_USO == constants.Vocation.MS:
             pyautogui.press('9') # utura gran
+            did_something = True
 
         
         
@@ -31,5 +35,9 @@ def manager_cacarecos(event):
         if pyautogui.locateOnScreen('imgs/battle_region_empty.png', confidence=0.8, region=constants.REGION_BATTLE) and not pyautogui.locateOnScreen('imgs/haste.png', confidence=0.95):
             print("vai dar haste")
             pyautogui.press('f10') # utani hur
+            did_something = True
+        
+        if did_something: # if an action was taken we safe to sleep for a bit, no need to keep runing it so often
+            time.sleep(20)
 
-        time.sleep(20) # there is no need to hammer this ad eternum
+        
